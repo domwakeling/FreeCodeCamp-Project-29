1b07baf708a86dea72903b31a52201cd56d88309
Dom Wakeling <domwakeling@gmail.com>
2018-04-27 22:42:57 +0100

    Added a filter dropdown to home page

--- imports/ui/pages/Home.jsx
+++ imports/ui/pages/Home.jsx
@@ -5,7 +5,14 @@ import PropTypes from 'prop-types';
 import { Bert } from 'meteor/themeteorchef:bert';
 import Books from '../../api/books.js';
 
+const filterModes = ['All books', 'My books', 'My trades'];
+
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { filterMode: 0 };
+    }
+
     async removeHandler(event, _id) {
         event.preventDefault();
         await Meteor.call(
@@ -25,39 +32,55 @@ class Home extends React.Component {
         this.props.history.push('/addbook');
     }
 
+    filterHandler(e, idx) {
+        e.preventDefault();
+        this.setState({ filterMode: idx });
+    }
+
     renderBooks() {
         this.removeHandler = this.removeHandler.bind(this);
         return (
             <div className="books-container">
-                {this.props.books.map(book => (
-                    <div key={book.bookId} className="book-display">
-                        {this.props.user && book.user !== this.props.user._id ? (
-                            <button
-                                className="main-button"
-                                onClick={e => console.log(e)}
-                            >
-                                Propose trade
-                            </button>
-                        ) : '' }
-                        {this.props.user && book.user === this.props.user._id ? (
-                            <button
-                                className="main-button remove-button"
-                                onClick={(e) => { this.removeHandler(e, book._id); }}
-                            >
-                                Remove book
-                            </button>
-                        ) : ''}
-                        <img src={book.imageURL} alt="" />
-                    </div>
-                ))}
+                {this.props.books
+                    .filter((book) => {
+                        if (this.state.filterMode === 1) {
+                            return book.user === this.props.user._id;
+                        } else if (this.state.filterMode === 2) {
+                            // TODO - add filter for trades
+                            return true;
+                        }
+                        return true;
+                    })
+                    .map(book => (
+                        <div key={book.bookId} className="book-display">
+                            {this.props.user && book.user !== this.props.user._id ? (
+                                <button
+                                    className="main-button"
+                                    onClick={e => console.log(e)}
+                                >
+                                    Propose trade
+                                </button>
+                            ) : '' }
+                            {this.props.user && book.user === this.props.user._id ? (
+                                <button
+                                    className="main-button remove-button"
+                                    onClick={(e) => { this.removeHandler(e, book._id); }}
+                                >
+                                    Remove book
+                                </button>
+                            ) : ''}
+                            <img src={book.imageURL} alt="" />
+                        </div>
+                    ))
+                }
             </div>
         );
     }
 
-
     render() {
         this.renderBooks = this.renderBooks.bind(this);
         this.addClickHandler = this.addClickHandler.bind(this);
+        this.filterHandler = this.filterHandler.bind(this);
         return (
             <div>
                 <h2>Home</h2>
@@ -69,6 +92,25 @@ class Home extends React.Component {
                         >
                             Add a book
                         </button>
+                        <div className="dropdown">
+                            <button id="which-books" className="main-button">
+                                {filterModes[this.state.filterMode]}
+                            </button>
+                            <div className="dropdown-content">
+                                {/* eslint-disable-next-line */}
+                                <li onClick={(e) => { this.filterHandler(e, 0); }}>
+                                    {filterModes[0]}
+                                </li>
+                                {/* eslint-disable-next-line */}
+                                <li onClick={(e) => { this.filterHandler(e, 1); }}>
+                                    {filterModes[1]}
+                                </li>
+                                {/* eslint-disable-next-line */}
+                                <li onClick={(e) => { this.filterHandler(e, 2); }}>
+                                    {filterModes[2]}
+                                </li>
+                            </div>
+                        </div>
                     </div>
                 ) : ''}
                 {this.renderBooks()}
